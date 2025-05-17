95e63889c5065069e184ec47df03e1280712bc66
itspitso <pitsomashilo1@gmail.com>
2025-05-17 10:30:10 +0200

    Added styling and functionality to the set number of questions screen

--- admin/script.js
+++ admin/script.js
@@ -13,12 +13,40 @@ document.querySelectorAll(".category-btn").forEach(button => {
     });
 });
 
-backBtn.addEventListener("click", () => {
-    categoryScreen.classList.remove("hidden");
-    optionsScreen.classList.add("hidden");
-    });
-});
-
 const setNumberOfQuestionsBtn = document.querySelector(".set-btn");
 const addQuestionBtn = document.querySelector(".add-btn");
 const deleteQuestionButton = document.querySelector(".del-btn");
+const setQuestionsScreen = document.getElementById("set-questions-screen");
+const optionButtons = document.querySelector(".option-buttons");
+
+setNumberOfQuestionsBtn.addEventListener("click", () => {
+    setQuestionsScreen.classList.remove("hidden");
+    optionButtons.classList.add("hidden");
+});
+
+backBtn.addEventListener("click", () => {
+    categoryScreen.classList.remove("hidden");
+    optionsScreen.classList.add("hidden");
+    setQuestionsScreen.classList.add("hidden");
+    optionButtons.classList.remove("hidden");
+    confirmationMsg.textContent = "";
+    });
+});
+
+let numberOfQuestions;
+const confirmationMsg = document.getElementById("confirm-msg");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("number-form");
+    const numberInput = document.getElementById("number-of-questions");
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        numberOfQuestions = parseInt(numberInput.value);
+
+        confirmationMsg.textContent = `Number ${numberOfQuestions} has been saved successfully!`;
+
+        form.reset();
+    });
+});
